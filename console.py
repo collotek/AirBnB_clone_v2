79f81607032e0ebfba66a46970c98dd1a495ba76
Enidbii <chebsenid@gmail.com>
2023-06-24 11:10:12 +0000

    args update

--- console.py
+++ console.py
@@ -121,7 +121,7 @@ class HBNBCommand(cmd.Cmd):
             list_arg = args.split(" ")
             kw = {}
             for arg in list_arg[1:]:
-                arg_splited = arg.split("=")
+                arg_splited = args.split("=")
                 arg_splited[1] = eval(arg_splited[1])
                 if type(arg_splited[1]) is str:
                     arg_splited[1] = arg_splited[1].replace("_", " ")\
@@ -129,8 +129,7 @@ class HBNBCommand(cmd.Cmd):
                 kw[arg_splited[0]] = arg_splited[1]
         except SyntaxError:
             print("** class name missing **")
-        except NameError:
-            print("** class doesn't exist **")
+
         new_instance = HBNBCommand.classes[list_arg[0]](**kw)
         new_instance.save()
         print(new_instance.id)
