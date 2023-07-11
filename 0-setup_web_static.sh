9cf8189715ff25f9a542151e1c1cd8fe915b373b
Enidbii <chebsenid@gmail.com>
2023-07-11 15:15:41 +0300

    yes

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+# sets up webservers for deployment of web_static
+
+exists()
+{
+  command -v "$1" >/dev/null 2>&1
+}
+if ! [exists nginx];then
+	sudo apt-get -y update
+	sudo apt-get -y install nginx
+fi
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared
+echo "nginx is intersting" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -hR ubuntu:ubuntu /data
+
+printf %s "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By $HOSTNAME;
+    root   /var/www/html;
+    index  index.html index.htm;
+
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+
+    location /redirect_me {
+        return 301 http://enidchebet.tech/;
+    }
+
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}" > /etc/nginx/sites-available/default
+
+service nginx restart
