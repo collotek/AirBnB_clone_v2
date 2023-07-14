fc3056d83b7d26ddf6053334d0eb3e0e7ba03b31
collotek <collinskipkorir94@gmail.com>
2023-07-14 16:04:10 +0300

    adding exception

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -6,11 +6,12 @@ from datetime import datetime
 
 def do_pack():
     """Local execute command to generate a .tgz archive"""
-    local("mkdir -p versions")
-    current_day = datetime.now()
-    current_time = current_day.strftime("%Y%m%d%H%M%S")
-    location = "versions/web_static_{}.tgz".format(current_time)
-    local_name = local("tar -czvf {} web_static".format(location))
-    if local_name.succeeded is False:
+    try:
+        local("mkdir -p versions")
+        current_day = datetime.now()
+        current_time = current_day.strftime("%Y%m%d%H%M%S")
+        location = "versions/web_static_{}.tgz".format(current_time)
+        local_name = local("tar -czvf {} web_static".format(location))
+        return local_name
+    except Exception:
         return None
-    return local_name
