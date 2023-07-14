8f0ee9f6b4807e34b5bdaa4fdbef67b379ab85e1
collotek <collinskipkorir94@gmail.com>
2023-07-14 16:00:05 +0300

    archive web static

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -10,7 +10,7 @@ def do_pack():
     current_day = datetime.now()
     current_time = current_day.strftime("%Y%m%d%H%M%S")
     location = "versions/web_static_{}.tgz".format(current_time)
-    local_name = ("tar -czvf web_static {} web_static".format(location))
+    local_name = local("tar -czvf {} web_static".format(location))
     if local_name.succeeded is False:
         return None
     return local_name
