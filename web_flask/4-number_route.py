e084724fd30bea447c099203af9496b60fc08daf
collotek <collinskipkorir94@gmail.com>
2023-07-26 00:27:47 +0300

    a script that starts a Flask web application

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Starts flask app and routes two requests
+
+    Returns:
+        string: normal strings
+    """
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def home():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def hbnb():
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def c(text=None):
+    return "C " + text.replace("_", " ")
+
+
+@app.route('/python/', defaults={'text': 'is cool'})
+@app.route('/python/<text>')
+def python(text):
+    return "Python " + text.replace("_", " ")
+
+
+@app.route('/number/<int:n>')
+def number(n):
+    return '{} is a number'.format(n)
+
+
+if __name__ == "__main__":
+    app.run()
